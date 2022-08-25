2e236ca8e73cdeb8efb5b9d0185764e3f67aa595
Suman Debnath <sumanhomingnote@gmail.com>
2022-08-26 02:55:32 +0530

    audit big fixes

--- routes/v1/audit/data_audit.js
+++ routes/v1/audit/data_audit.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const mongo = require("mongoose");
 const helper = require("../../../helper/helper");
 const logger = require('../../../helper/logger');
-const moment = require('moment');
 const br = helper.baseResponse;
 const router = new express.Router();
-const {bulkUploader, bondAttachUploader} = require('../helper/file_uploader');
-const SecurityGroupModel = require('../../../models/configSecurityGroupModel');
-const CurrencyModel = require('../../../models/configCurrencyModel');
-const CalenderOrBankHolidayModel = require('../../../models/configCalenderOrBankHolidayModel');
-const IbExchangeModel = require('../../../models/configIbExchangeModel');
-const IbQuote = require('../../../models/configIbQuoteModel');
-const IbParty = require('../../../models/configIbPartyModel');
-const IbInterestType = require('../../../models/configIbInterestTypeModel');
-const ReferenceRateModel = require('../../../models/configReferenceRatesDescriptionModel');
 const BondSecurityModel = require('../../../models/bondSecurityModel');
 const BondSecurityAuditModel = require('../../../models/bondSecurityAuditModel');
-const {Validator} = require('node-input-validator');
-const json2csv = require('json2csv').parse;
-const {processBulkInsert} = require('../helper/process_bulk_insert');
 const {authUser, isValidParamId} = require('../../../middleware/auth');
 const bondSecurityMiddleware = require('../../../middleware/bond_security_middleware');
 
@@ -29,9 +15,9 @@ const bondSecurityMiddleware = require('../../../middleware/bond_security_middle
  *      summary: Get all Audits
  *      tags: [Audit]
  *      parameters:
- *      - name: auditType
+ *      - name: auditFor
  *        in: query
- *        description: Audit Type
+ *        description: Audit For Table name
  *        default: bond
  *      - name: auditedUser
  *        in: query
@@ -54,37 +40,125 @@ const bondSecurityMiddleware = require('../../../middleware/bond_security_middle
 router.get("/get-all", authUser, bondSecurityMiddleware.canRead, async (req, res) => {
     try {
         let allAudits = [
-            "config_ab_framework_audits",
-            "config_accounting_calender_audits",
-            "config_accounting_period_definition_audits",
-            "config_accounting_treatment_lookup_audits",
-            "config_calender_or_bank_holiday_rule_audits",
-            "config_company_portfolio_audits",
-            "config_cost_basis_rule_audits",
-            "config_currency_audits",
-            "config_ib_asset_class_audits",
-            "config_ib_company_audits",
-            "config_ib_exchange_audits",
-            "config_ib_interest_type_audits",
-            "config_ib_party_audits",
-            "config_ib_quote_audits",
-            "config_ib_structure_audits",
-            "config_ib_transaction_status_audits",
-            "config_ledger_lookup_audits",
-            "config_ledger_period_control_audits",
-            "config_portfolio_group_audits",
-            "config_portfolio_type_audits",
-            "config_price_audits",
-            "config_reference_rates_description_audits",
-            "config_rounding_table_audits",
-            "config_security_group_audits",
-            "config_transaction_code_audits"
+            {
+                searchFilter: "Ab Framework",
+                tableName: "config_ab_framework_audits"
+            },
+            {
+                searchFilter: "Accounting Calender",
+                tableName: "config_accounting_calender_audits"
+            },
+            {
+                searchFilter: "Accounting Period Definition",
+                tableName: "config_accounting_period_definition_audits"
+            },
+            {
+                searchFilter: "Accounting Treatment Lookup",
+                tableName: "config_accounting_treatment_lookup_audits"
+            },
+            {
+                searchFilter: "Calender Or Bank Holiday Rule",
+                tableName: "config_calender_or_bank_holiday_rule_audits"
+            },
+            {
+                searchFilter: "Company Portfolio",
+                tableName: "config_company_portfolio_audits"
+            },
+            {
+                searchFilter: "Cost Basis Rule",
+                tableName: "config_cost_basis_rule_audits"
+            },
+            {
+                searchFilter: "Currency",
+                tableName: "config_currency_audits"
+            },
+            {
+                searchFilter: "Ib Asset Class",
+                tableName: "config_ib_asset_class_audits"
+            },
+            {
+                searchFilter: "Ib Company",
+                tableName: "config_ib_company_audits"
+            },
+            {
+                searchFilter: "Ib Exchange",
+                tableName: "config_ib_exchange_audits"
+            },
+            {
+                searchFilter: "Ib Interest Type",
+                tableName: "config_ib_interest_type_audits"
+            },
+            {
+                searchFilter: "Ib Party",
+                tableName: "config_ib_party_audits"
+            },
+            {
+                searchFilter: "Ib Quote",
+                tableName: "config_ib_quote_audits"
+            },
+            {
+                searchFilter: "Ib Structure",
+                tableName: "config_ib_structure_audits"
+            },
+            {
+                searchFilter: "Ib Transaction Status",
+                tableName: "config_ib_transaction_status_audits"
+            },
+            {
+                searchFilter: "Ledger Lookup",
+                tableName: "config_ledger_lookup_audits"
+            },
+            {
+                searchFilter: "Ledger Period Control",
+                tableName: "config_ledger_period_control_audits"
+            },
+            {
+                searchFilter: "Portfolio Group",
+                tableName: "config_portfolio_group_audits"
+            },
+            {
+                searchFilter: "Portfolio Type",
+                tableName: "config_portfolio_type_audits"
+            },
+            {
+                searchFilter: "Price",
+                tableName: "config_price_audits"
+            },
+            {
+                searchFilter: "Reference Rates Description",
+                tableName: "config_reference_rates_description_audits"
+            },
+            {
+                searchFilter: "Rounding Table",
+                tableName: "config_rounding_table_audits"
+            },
+            {
+                searchFilter: "Security Group",
+                tableName: "config_security_group_audits"
+            },
+            {
+                searchFilter: "Transaction Code",
+                tableName: "config_transaction_code_audits"
+            },
+            {
+                searchFilter: "Bond",
+                tableName: "bond_securities"
+            }
         ];
+        let searchFilters = allAudits.map(a => a.searchFilter.toLowerCase());
+        let excludeBondItems = false;
+
+        if(req.query.auditFor !== undefined && req.query.auditFor !== null && req.query.auditFor.length > 0){
+            let searchItems = req.query.auditFor.split(',');
+            let needToFilterItems = searchFilters.filter( a => searchItems.includes(a.toString().toLowerCase()));
+            searchFilters = needToFilterItems.length > 0 ? needToFilterItems : searchFilters;
+            excludeBondItems = searchFilters.includes('bond');
+        }
 
         let pipeline = [
             {
                 '$addFields': {
-                    'collectionName': 'bond_security_audits'
+                    'collectionName': 'Bond'
                 }
             }
         ];
@@ -96,44 +170,43 @@ router.get("/get-all", authUser, bondSecurityMiddleware.canRead, async (req, res
             });
         }
 
+        console.log(searchFilters);
         allAudits.forEach((au) => {
-            let filterPipeline = [
-                {
-                    '$addFields': {
-                        'collectionName': au
+            if(searchFilters.includes(au.searchFilter) && au.searchFilter !== 'bond'){
+                let filterPipeline = [
+                    {
+                        '$addFields': {
+                            'collectionName': au.searchFilter
+                        }
                     }
+                ];
+                if (helper.isValidObjectId(req.params.auditedUser)) {
+                    filterPipeline.push({
+                        '$match': {
+                            'actionBy': req.params.auditedUser
+                        }
+                    });
                 }
-            ];
-            if (helper.isValidObjectId(req.params.auditedUser)) {
-                filterPipeline.push({
-                    '$match': {
-                        'actionBy': req.params.auditedUser
+                pipeline.push({
+                    '$unionWith': {
+                        'coll': au.tableName,
+                        'pipeline': filterPipeline
                     }
                 });
             }
-            pipeline.push({
-                '$unionWith': {
-                    'coll': au,
-                    'pipeline': filterPipeline
-                }
-            });
         });
 
-/*        if (req.query.searchKey !== undefined && eligibleSearchKeys.includes(req.query.searchKey.toString())) {
-            let searchKey = req.query.searchKey.toString();
-            let searchData = req.query.search !== undefined ? req.query.search.toString() : '';
-            logger.info(`SearchKey: ${req.query.searchKey} => ${req.query.search}`);
-
-            filter[searchKey] = {
-                $regex: '/^' + searchData + '/i',
-            }
-        }*/
-
-        /*        if (req.query.search !== undefined && req.query.search.length > 0) {
-                    filter.costBasisProfileName = {
-                        $regex: '/^' + req.query.search + '/i',
+        //exclude bond items
+        if(excludeBondItems){
+            pipeline.push({
+                $match : {
+                    collectionName: {
+                        $nin : [ 'Bond' ]
                     }
-                }*/
+                }
+            });
+        }
+
         let result = {
             total: 0,
             perPage: 20,
@@ -154,9 +227,10 @@ router.get("/get-all", authUser, bondSecurityMiddleware.canRead, async (req, res
         }
 
         console.log(JSON.stringify(pipeline));
-        result.total = await BondSecurityModel.aggregate([...pipeline, {
+        result.total = await BondSecurityAuditModel.aggregate([...pipeline, {
             '$count': 'count'
         }]);
+        console.log(result);
         result.total = result.total[0].count;
 
         pipeline.push({
@@ -168,7 +242,7 @@ router.get("/get-all", authUser, bondSecurityMiddleware.canRead, async (req, res
         let offset = (result.currentPage - 1) * result.perPage;
         result.lastPage = lp > 1 ? lp : null;
         result.nextPage = result.total > (result.perPage * result.currentPage) ? result.currentPage + 1 : null;
-        result.data = await BondSecurityModel.aggregate([...pipeline, {
+        result.data = await BondSecurityAuditModel.aggregate([...pipeline, {
             '$skip': offset
         }, {
             '$limit': result.perPage
@@ -182,6 +256,7 @@ router.get("/get-all", authUser, bondSecurityMiddleware.canRead, async (req, res
 
         br.sendSuccess(res, result);
     } catch (error) {
+        console.log(error);
         logger.error(error);
         br.sendServerError(res, {});
     }
